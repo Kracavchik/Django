292c3988ed9f7758b4c600f7837c09d94c1e1359
Kracavchik <sir.pasha1512@yandex.ru>
2020-05-11 09:12:31 +0300

    Homework done

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,14 +1,33 @@
+import datetime
+import json
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def read_json(name):
+    path = "mainapp/json/" + name + '.json'
+    with open(path, 'r', encoding="utf-8") as f:
+        data = json.loads(f.read())
+        return data
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = "главная"
+    products = read_json('main').values()
+    content = {"title": title, "products": products}
+    return render(request, "mainapp/index.html", content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = "продукты"
+    links_menu = read_json('products').get('a')
+    same_products = read_json('products').get('b')
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
 
 
-def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+def contact(request):
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+    locations = read_json('contact').get('a')
+    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    return render(request, "mainapp/contact.html", content)
