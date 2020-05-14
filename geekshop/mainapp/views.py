7eebe47a9bd853299ab95ce3f19b7bfe81b85d13
Kracavchik <sir.pasha1512@yandex.ru>
2020-05-14 22:58:38 +0300

    Homework done

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,33 +1,51 @@
-import datetime
-import json
-
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import timezone
 
+from .models import Product, ProductCategory
 
-def read_json(name):
-    path = "mainapp/json/" + name + '.json'
-    with open(path, 'r', encoding="utf-8") as f:
-        data = json.loads(f.read())
-        return data
 
 def main(request):
     title = "главная"
-    products = read_json('main').values()
-    content = {"title": title, "products": products}
+
+    products = Product.objects.all()
+
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
-def products(request):
+def products(request, pk=None):
     title = "продукты"
-    links_menu = read_json('products').get('a')
-    same_products = read_json('products').get('b')
-    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    content = {
+        "title": title,
+        "links_menu": links_menu,
+        "same_products": same_products,
+        "media_url": settings.MEDIA_URL,
+    }
+    if pk:
+        print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
-    visit_date = datetime.datetime.now()
-    locations = read_json('contact').get('a')
+    visit_date = timezone.now()
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
